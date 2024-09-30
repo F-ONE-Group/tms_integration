b9b724926ed3c3304e4563d0993be9088bc941d4
Alessandro Mosca <a.mosca@f-one.group>
2024-09-30 15:13:04 +0300

    add dms import

--- tms_integration/lis_winsped/lis_winsped.py
+++ tms_integration/lis_winsped/lis_winsped.py
@@ -1,4 +1,5 @@
 import tempfile
+from typing import List
 from pydantic.dataclasses import dataclass
 from tms_integration.utils.sftp import SftpBase
 
@@ -20,6 +21,24 @@ class LisWinSped(SftpBase):
             tmp_file.write(payload.generate_txt())
             tmp_file.close()
             self.import_file(tmp_file.name, self.import_dest_folder)
+
+    def import_documents(
+        self, dms_payload, files: List[str], import_prefix: str = None
+    ):
+        with tempfile.NamedTemporaryFile(
+            mode="w",
+            encoding="utf-8",
+            prefix=import_prefix,
+            suffix=".txt",
+            delete=False,
+        ) as tmp_file:
+            tmp_file.write(dms_payload.generate_txt())
+            tmp_file.close()
+            self.import_file(tmp_file.name, self.import_dest_folder)
+
+        # send the files as well
+        for file in files:
+            self.import_file(file, self.import_dest_folder)
 
     def export_auftrag(self):
         raise NotImplementedError
