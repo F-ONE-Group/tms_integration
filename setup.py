78b23fa252d4b69697ca24b1795790ac38e1df11
Alessandro Mosca <a.mosca@f-one.group>
2025-05-19 09:12:16 +0300

    0.1.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ carlo_requirements = get_requirements(
 
 setup(
     name="tms_integration",
-    version="0.1.1",
+    version="0.1.2",
     description="A library for TMS integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
