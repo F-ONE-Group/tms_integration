8a071338ec1d2711a3732e3457e499e8ec48e607
Alessandro <a.mosca@f-one.group>
2026-02-11 19:43:10 +0300

    bump version to 0.2.4

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ carlo_requirements = get_requirements(
 
 setup(
     name="tms_integration",
-    version="0.2.3",
+    version="0.24",
     description="A library for TMS integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
