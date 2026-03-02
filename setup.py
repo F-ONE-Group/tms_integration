a294e4444f0ab12419e69becff42fa9dec2afc4f
artemfadin <a.fadin@f-one.group>
2026-03-02 17:41:33 +0100

    bump up version to 1.0.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ windispo_requirements = get_requirements(
 
 setup(
     name="tms_integration",
-    version="0.3.1",
+    version="1.0.0",
     description="A library for TMS integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
