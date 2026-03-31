acc6c83e854bdb899570a91085238971dbb233ce
Artem <84914756+artemfadin@users.noreply.github.com>
2026-03-31 17:38:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ windispo_requirements = get_requirements(
 
 setup(
     name="tms_integration",
-    version="1.1.2",
+    version="1.1.3",
     description="A library for TMS integration",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
